479315f86bb0e537d93b488bef72a1c0f35a7d87
MrSaltyBananaMan <hardyfcathal@gmail.com>
2023-11-07 13:34:43 +0000

    Created a method which includes the loop

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -1,16 +1,19 @@
 package ie.atu;
 
+
 public class ArrayDemo {
+    public static void outputElements(int[] x){
+        for(int i=0;i<10;i++){
+            x[i] = 100 + i * 100;
+            System.out.println("Element at index " + i + ": " + x[i]);
+        }
+    }
     public static void main(String[] args) {
         int[] array;
-        int i;
 
         array = new int[10];
 
-        for(i=0;i<10;i++){
-            array[i] = 100 + i * 100;
-            System.out.println("Element at index " + i + ": " + array[i]);
-        }
+        outputElements(array);
 
     }
 }
